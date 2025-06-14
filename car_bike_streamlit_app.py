b4a5925700964235c39a1614e4e458d7cf5d45fa
qwertyasdzxcrty <113295307+qwertyasdzxcrty@users.noreply.github.com>
2025-06-14 17:11:11 +0700

    Add files via upload

--- car_bike_streamlit_app.py
+++ car_bike_streamlit_app.py
@@ -1,10 +1,16 @@
-
 import streamlit as st
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
+
+import sys
+print(sys.path)  # Check paths
+import tensorflow as tf
+print(tf.__version__)  # Confirm TensorFlow is accessible
+
 
 st.title("Car vs Bike Image Classifier")
 
@@ -16,7 +22,7 @@ def load_trained_model():
 
 model = load_trained_model()
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader("Choose an image...", type=s["jpg", "png", "jpeg"])
 if uploaded_file is not None:
     img = Image.open(uploaded_file).resize((150, 150))
     st.image(img, caption='Uploaded Image', use_column_width=True)
